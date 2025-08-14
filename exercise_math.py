a30c3e7900313511cd321ab3fc2fcb4e445504c7
bdistefanoo <bdistefano@mail.austral.edu.ar>
2025-08-14 11:44:36 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,42 +1,4 @@
 def math():
-    i1 = 3
-    i2 = 5
-    i3 = i2 + i1
-    print("valor de i1:")  # valor de i1:3
-    print(i1)  # 3
-    print("valor de i2:") # valor de i2:5
-    print(i2) # 5
-    print("valor de i3:") # valor de i3:8
-    print(i3) #8
-    print(i1 + i2 + i3) #16
-
-    s1, s2, s3 = "Python", " is ", 'awesome'
-    print(s1 + s2 + s3) #Python is awesome
-
-    x = y = z = "Naranja"
-    print("valor de x: " + x + ", valor de y: " + y + ", valor de z: " + z) # valor de x: Naranja, valor de y: Naranja, valor de z: Naranja
-
-    z1 = i3 / i2
-    print(z1) # 1.6
-    z2 = i3 % i2
-    print(z2) # 3
-    f1 = -.5
-    f2 = 10
-    f3 = f1 + f2
-    i3 = int(f3)
-    print("entero i3:") # 9
-    print(i3)
-    print("variable f3:") # 9.5
-    print(f3)
-    f2 += i1
-    print("el valor de") # el valor de
-    print(f2) #13
-    print("más") # mas
-    print(f1) # - 0.5
-    print("es:") # es:
-    print(f2 + f1) # 12.5
-    
-
     a = 57
     b = 7
     suma = a + b
